83dd6d2fb2533f7d5bbe40d1a8eb446dbf54f82f
harapr-fds <philip.haratsaris@formationds.com>
2016-03-21 09:23:55 -0600

    Modifies setup.py to yield source distribution that will work with pip install

--- setup.py
+++ setup.py
@@ -3,15 +3,6 @@
 from sys import platform
 from distutils.core import setup, Extension
 
-setup(name='PYSCSI',
-      version='1.0',
-      license='LGPLv2.1',
-      author='Ronnie Sahlberg',
-      author_email='ronniesahlberg@gmail.com',
-      description='Module for calling SCSI devices from Python',
-      packages=['pyscsi', 'pyscsi.pyscsi', 'pyscsi.utils'])
-
-
 #
 # SGIO is linux specific
 #
@@ -19,11 +10,20 @@ if platform[:5] == 'linux':
     linux_sgio_module = Extension('linux_sgio.linux_sgio',
                                   sources=['linux_sgio/sgiomodule.c'])
 
-    setup(name='LINUX_SGIO',
+    setup(name='pyscsi-linux',
           version='1.0',
           license='LGPLv2.1',
           author='Ronnie Sahlberg',
           author_email='ronniesahlberg@gmail.com',
-          description='Module for calling Linux SG_IO devices',
-          packages=['linux_sgio'],
+          description='Module for calling SCSI devices from Python, Python binding for Linux SCSI Generic (sg) Driver',
+          packages=['pyscsi', 'pyscsi.pyscsi', 'pyscsi.utils', 'linux_sgio'],
           ext_modules=[linux_sgio_module])
+
+else:
+    setup(name='pyscsi',
+          version='1.0',
+          license='LGPLv2.1',
+          author='Ronnie Sahlberg',
+          author_email='ronniesahlberg@gmail.com',
+          description='Module for calling SCSI devices from Python',
+          packages=['pyscsi', 'pyscsi.pyscsi', 'pyscsi.utils'])
